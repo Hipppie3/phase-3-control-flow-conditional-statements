5014c66a6c2b1b842c0e5c0a405972762f6513b5
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2022-10-30 20:46:13 -0600

    commit tree c482054d327544ae3bcbcfe9f4739f340fe68524

--- js/index.js
+++ js/index.js
@@ -1,71 +1,61 @@
-/*
-  Write a method `admin_login` that takes two arguments, a username and a
-  password. If the username is "admin" or "ADMIN" and the password is "12345",
-  return "Access granted". Otherwise, return "Access denied".
-*/
-function adminLogin(username, password) {
-  if ((username === "admin" || username === "ADMIN") && password === "12345") {
-    return "Access granted";
-  } else {
-    return "Access denied";
-  }
-}
+def admin_login(username, password)
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
+end
 
-/*
-  Write a method `hows_the_weather` that takes in one argument, a temperature.
-  If the temperature is below 40, return "It's brisk out there!". If the temperature is
-  between 40 and 65, return "It's a little chilly out there!". If the temperature is above 85,
-  return "It's too dang hot out there!". Otherwise, return "It's perfect out there!"
-*/
-function howsTheWeather(temperature) {
-  let response;
-  if (temperature < 40) {
-    response = "brisk";
-  } else if (temperature >= 40 && temperature <= 65) {
-    response = "a little chilly";
-  } else if (temperature > 85) {
-    response = "too dang hot";
-  } else {
-    response = "perfect";
-  }
-  return `It's ${response} out there!`;
-}
+def hows_the_weather(temperature)
+  response =  if temperature < 40
+                "brisk"
+              elsif temperature >= 40 && temperature <= 65
+                "a little chilly"
+              elsif temperature > 85
+                "too dang hot"
+              else
+                "perfect"
+              end
 
-/* 
-  Write a method `fizzbuzz` takes in a number. For multiples of three, return
-  "Fizz" instead of the number. For the multiples of five, return "Buzz". For
-  numbers which are multiples of both three and five, return "FizzBuzz". For
-  all other numbers, just return the number itself.
-*/
-function fizzbuzz(num) {
-  if (num % 3 === 0 && num % 5 === 0) {
-    return "FizzBuzz";
-  } else if (num % 3 === 0) {
-    return "Fizz";
-  } else if (num % 5 === 0) {
-    return "Buzz";
-  } else {
-    return num;
-  }
-}
+  "It's #{response} out there!"
+end
 
-/*
-  Write a method `calculator` that takes three arguments: an operation and two
-  numbers. If the operation is one of the following: `+`, `-`, `*`, or `\`,
-  return the value of calling the operation on the two numbers. Otherwise,
-  output a message saying "Invalid operation!" and return `nil`.
-*/
-function calculator(operation, num1, num2) {
-  switch (operation) {
-    case "+":
-      return num1 + num2;
-    case "-":
-      return num1 - num2;
-    case "*":
-      return num1 * num2;
-    case "/":
-      return num1 / num2;
-    default:
-      console.log("Invalid operation!");
-  }
-}
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
+end
+
+def calculator(operation, num1, num2)
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+  end
+end
+
+# Here's an alternate solution for calculator with some fun Ruby metaprogramming
+def meta_calculator(operation, num1, num2)
+  if ["+", "-", "*", "/"].include?(operation)
+    # the .send method lets you use a string or a symbol as a method
+    # so, the line below translates to 4.send("/", 2)
+    # which is equivalent to 4 / 2
+    # https://stackoverflow.com/a/3337954
+    num1.send(operation, num2)
+  else
+    puts "Invalid operation!"
+  end
+end
